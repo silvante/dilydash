430777552b09608c3a1658a806a682bea3b0c99f
silvante <khamidov.ko@gmail.com>
2024-05-04 20:39:18 +0500

    packages

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        base: "#f2f4f5",
+        greeny: "#00c65e"
+      },
+      width: {
+        base: "600px",
+        norm: "300px",
+      },
+    },
   },
   plugins: [],
-}
+};
